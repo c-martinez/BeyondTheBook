06fc05bd9bbec40591f0bbf29307dcdaac10c4c4
Carlos Martinez <c.martinez@esciencecenter.nl>
2015-03-13 15:25:02 +0100

    INL script -- break into sentences

--- inlScript.py
+++ inlScript.py
@@ -1,6 +1,6 @@
 """Summarize book.
 
-Usage: inlScript.py <teiFile> <liwcOutput> <semOutput> <countsOutput>
+Usage: inlScript.py <data>
                     [--langModel MODEL] [--liwcFile LIWCFile]
                     [--min-commonness <minc>] [ -h | --help]
 
@@ -8,7 +8,7 @@ Options:
   --langModel MODEL        Semanticizer language model [default: nl.model]
   --liwcFile LIWCFile      LIWC word file [default: LIWC2007_Dutch.dic.html]
   --min-commonness <minc>  Minimum commonness of semanticized entities
-                           for them to be considered [default: 0.5]
+                           for them to be considered [default: 0.1]
 
 """
 from __future__ import division
@@ -16,6 +16,7 @@ from __future__ import division
 import BeautifulSoup
 import pickle
 import nltk
+import glob
 
 from collections import defaultdict
 from semanticizest import Semanticizer
@@ -45,26 +46,11 @@ def invertSearchDict(liwcDict):
             wordCategory[word].append(cat)
     return dict(wordCategory)
 
-if __name__ == '__main__':
-    arguments = docopt(__doc__, version='NLeSc book summarization')
-    teiFile = arguments['<teiFile>']
-    langModel = arguments['--langModel']
-    liwcFile = arguments['--liwcFile']
-    commonnessThreshold = arguments['--min-commonness']
-    liwcListFile = arguments['<liwcOutput>']
-    semListFile = arguments['<semOutput>']
-    countsListFile = arguments['<countsOutput>']
-
-    liwcDict = loadLIWCDic(liwcFile)
-    wordDict = invertSearchDict(liwcDict)
-
-    sem = Semanticizer(langModel)
-    print 'Language model loaded'
-
+def processTeiFile(teiFile, liwcListFile, semListFile, countsListFile, commonnessThreshold, sent_tokenize):
     tei = open(teiFile).read()
     teiSoup = BeautifulSoup.BeautifulSoup(tei)
 
-    print 'Processing book...'
+    print 'Processing book...',teiFile
     nPara = 0
     semList = []
     liwcList = []
@@ -74,6 +60,7 @@ if __name__ == '__main__':
     # Book as a list of paragraphs
     # For each paragraph
 
+    print 'Begin iterate paragraphs: ',len(paras)
     for para in paras:
         pText = para.getText(separator=' ')
         nPara += 1
@@ -81,31 +68,61 @@ if __name__ == '__main__':
         pId = pId if pId is not None else nPara
         print '\r  paragraph: ',pId,
 
-        tokens = nltk.word_tokenize(pText)
+        # Count total words in paragraph -- Initialize
+        wordCount = 0
 
-        # Count total words in paragraph
-        countsList.append((pId, len(tokens)))
+        sents = sent_tokenize.tokenize(pText)
+        for sent in sents:
+            tokens = nltk.word_tokenize(sent)
 
-        # Semanticize and keep list of words
-        candidates = sem.all_candidates(tokens)
-        for i0,i1,wikiTitle,commonness in candidates:
-            if commonness>commonnessThreshold:
-                word = ' '.join(tokens[i0:i1])
-                semList.append((pId, word, wikiTitle, commonness))
+            # Count total words in paragraph -- Increment
+            wordCount += len(tokens)
 
-        # Identify and count LIWC words
-        for word in tokens:
-            word = word.lower()
-            if word in wordDict:
-                liwcList.append((pId, word, wordDict[word]))
+            # Semanticize and keep list of words
+            candidates = sem.all_candidates(tokens)
+            for i0,i1,wikiTitle,commonness in candidates:
+                if commonness>commonnessThreshold:
+                    word = ' '.join(tokens[i0:i1])
+                    semList.append((pId, word, wikiTitle, commonness))
+
+            # Identify and count LIWC words
+            for word in tokens:
+                word = word.lower()
+                if word in wordDict:
+                    liwcList.append((pId, word, wordDict[word]))
+
+        # Count total words in paragraph -- save
+        countsList.append((pId, wordCount))
     print ''
 
     # Save in some format
     print 'Saving summary lists'
-    print '  %d LIWC entries created'%(len(liwcListFile))
-    print '  %d Semanticizer entries created'%(len(semListFile))
+    print '  %d LIWC entries created'%(len(liwcList))
+    print '  %d Semanticizer entries created'%(len(semList))
     print '  %d Paragraph count entries created'%(len(countsList))
     pickle.dump(liwcList, open(liwcListFile, 'w'))
     pickle.dump(semList, open(semListFile, 'w'))
     pickle.dump(countsList, open(countsListFile, 'w'))
 
+if __name__ == '__main__':
+    arguments = docopt(__doc__, version='NLeSc book summarization')
+    # teiFile = arguments['<teiFile>']
+    langModel = arguments['--langModel']
+    liwcFile = arguments['--liwcFile']
+    commonnessThreshold = arguments['--min-commonness']
+    dataDir = arguments['<data>']
+    commonnessThreshold = float(commonnessThreshold)
+
+    liwcDict = loadLIWCDic(liwcFile)
+    wordDict = invertSearchDict(liwcDict)
+    sem = Semanticizer(langModel)
+    print 'Language model loaded'
+
+    sent_tokenize = nltk.data.load('tokenizers/punkt/dutch.pickle')
+
+    for teiFile in glob.glob(dataDir + '*.xml'):
+        outFile = teiFile.replace(dataDir, 'output/')
+        liwcList = outFile.replace('.xml', '.liwc.pkl')
+        semList  = outFile.replace('.xml', '.sem.pkl')
+        countsList= outFile.replace('.xml', '.count.pkl')
+        processTeiFile(teiFile, liwcList, semList, countsList, commonnessThreshold, sent_tokenize)
